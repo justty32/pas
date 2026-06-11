433fa2afc3fc0527b0113d5e6908fb0f44c3d4ad
GuanYu Lu <guanyu.lu@teco.com.tw>
2026-06-11 08:28:48 +0800

    chore: 移除 derived/rimworld_mods（已遷移至 my_rimworld_mods repo）

--- derived/rimworld_mods/body-fortification-hediff/Source/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("BodyFortificationHediff")]
-[assembly: AssemblyProduct("BodyFortificationHediff")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
